f903f60d267cd1cf8d22868920c0691441012804
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-02-19 12:43:47 +0530

    feat: enhance seed script to include system user and add audit ledger records

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,7 @@ async function main() {
     // creating users
     await prisma.user.createMany({
         data: [
-            { email: "treasury@test.com", name: "Treasury" },
+            { email: "treasury@test.com", name: "Treasury", isSystem: true },
             { email: "userx@test.com", name: "userx" },
             { email: "usery@test.com", name: "usery" },
         ]
@@ -34,6 +34,23 @@ async function main() {
             { userId: 3, assetTypeId: 1, balance: 100 },
             { userId: 3, assetTypeId: 2, balance: 1000 },
             { userId: 3, assetTypeId: 3, balance: 100 },
+        ]
+    });
+
+    // creating audit ledger records
+    await prisma.auditLedger.createMany({
+        data: [
+            { transactionId: Bun.randomUUIDv7(), userId: 1, assetTypeId: 1, entryType: "credit", amount: 1000000 },
+            { transactionId: Bun.randomUUIDv7(), userId: 1, assetTypeId: 2, entryType: "credit", amount: 1000000 },
+            { transactionId: Bun.randomUUIDv7(), userId: 1, assetTypeId: 3, entryType: "credit", amount: 1000000 },
+
+            { transactionId: Bun.randomUUIDv7(), userId: 2, assetTypeId: 1, entryType: "credit", amount: 1000 },
+            { transactionId: Bun.randomUUIDv7(), userId: 2, assetTypeId: 2, entryType: "credit", amount: 1000 },
+            { transactionId: Bun.randomUUIDv7(), userId: 2, assetTypeId: 3, entryType: "credit", amount: 1000 },
+            
+            { transactionId: Bun.randomUUIDv7(), userId: 3, assetTypeId: 1, entryType: "credit", amount: 100 },
+            { transactionId: Bun.randomUUIDv7(), userId: 3, assetTypeId: 2, entryType: "credit", amount: 1000 },
+            { transactionId: Bun.randomUUIDv7(), userId: 3, assetTypeId: 3, entryType: "credit", amount: 100 },
         ]
     });
 
